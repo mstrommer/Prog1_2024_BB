ff3accf137226db7a9fc53172949ae722816c4c8
Michael Strommer <69633126+mstrommer@users.noreply.github.com>
2024-09-25 19:44:21 +0200

    Methods.java added.

---
+++ src/main/java/at/ac/fhcampuswien/Methods.java
@@ -0,0 +1,30 @@
+package at.ac.fhcampuswien;
+
+public class Methods {
+    public static void main(String[] args) {
+        System.out.println("=== Exercise 1 ===");
+        int number = 10;
+        printUntilNumber(number);
+
+        System.out.println("=== Exercise 2 ===");
+        System.out.println(sumOfNumbers(3,1000));
+    }
+
+    // Exercise 1 aus den Folien
+    public static void printUntilNumber(int number){
+        for(int count = 1; count <= number; count++){
+            System.out.println(count);
+        }
+    }
+
+    // Exercise 2 aus den Folien
+    public static int sumOfNumbers(int start, int end){
+        int sum = 0;
+        // Loop
+        for (int i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+}
